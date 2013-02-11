c0736137ed32dba83b72dc45506660ca944a486e
cenbek <sa.ordekci@gmail.com>
2013-02-11 10:53:18 +0200

    bootstarp project initialized.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootstrap::Application.config.secret_token = 'de00cb41ae7f54ae5541b38b6cb6859634a9fbcd2fce893cb2602ee3883afc27005b3d096f69ec4b19d71f726f00ae1c7384d80f0c0f727d03ced061c4c4fae5'
